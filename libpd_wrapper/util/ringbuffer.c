bab4830bfb83291912945304ea1e1e7f9a0f2f5f
David Tin Nyo <tinman@hackspanner.com>
2016-09-19 01:11:04 -0700

    msvc2015 support

--- libpd_wrapper/util/ringbuffer.c
+++ libpd_wrapper/util/ringbuffer.c
@@ -20,6 +20,7 @@
 # define SYNC_COMPARE_AND_SWAP(ptr, oldval, newval) \
          OSAtomicCompareAndSwap32Barrier(oldval, newval, ptr) 
 #elif defined(_MSC_VER) // win api atomics
+#include <windows.h>
 # define SYNC_FETCH(ptr) InterlockedOr(ptr, 0)
 # define SYNC_COMPARE_AND_SWAP(ptr, oldval, newval) \
          InterlockedCompareExchange(ptr, oldval, newval)
